f460edc031eaa04fbbe7381876a98f64c7ee7af3
Francisco-Fredes <francisco.a.fredes@gmail.com>
2024-05-24 11:33:11 -0400

    Agregando readme

--- db.sql
+++ db.sql
@@ -2,8 +2,11 @@
 CREATE DATABASE estudiantes;
 
 -- Crear tabla alumnos
-CREATE TABLE alumnos(
-    
+CREATE TABLE alumnos (
+    nombre VARCHAR(100),
+    rut VARCHAR(14),
+    curso VARCHAR(50),
+    nivel INT
 );
-
---
+-- Insertar un usuario default para probar
+INSERT INTO alumnos(nombre, rut, curso, nivel) VALUES ('Juan', '123.123-2', 'guitarra', 3);
